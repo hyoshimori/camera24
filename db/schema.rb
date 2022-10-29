32578f6a23a7c6e27a6b7a88999ccd1bbc41718e
Malte Geisen <geisenm64@gmail.com>
2022-10-29 18:02:30 +0200

    models

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_29_160155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cameras", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "location"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cameras_on_user_id"
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.bigint "camera_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["camera_id"], name: "index_offers_on_camera_id"
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "cameras", "users"
+  add_foreign_key "offers", "cameras"
+  add_foreign_key "offers", "users"
 end
